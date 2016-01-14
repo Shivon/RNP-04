17eaf74fc13a5b3a3018dfd7356f20927c14735c
Shivon <marjan.bachtiari@haw-hamburg.de>
2016-01-14 18:31:12 +0100

    update firewalling

--- firewalling/ban_network.sh
+++ firewalling/ban_network.sh
@@ -6,11 +6,11 @@ sudo /sbin/rcSuSEfirewall2 restart
 
 echo "ban input from 172.16.1.0/24"
 
-sudo /usr/sbin/iptables -I INPUT -s 172.16.1.0/24 -j DROP
+sudo /usr/sbin/iptables -I INPUT -d 172.16.1.0/24 -j DROP
 
 echo "ban output for 172.16.1.0/24"
 
-sudo /usr/sbin/iptables -I OUTPUT -d 172.16.1.0/24 -j DROP
+sudo /usr/sbin/iptables -I OUTPUT -s 172.16.1.0/24 -j DROP
 
 echo "finished banning 172.16.1.0/24"
 
--- firewalling/ban_tcp_server.sh
+++ firewalling/ban_tcp_server.sh
@@ -6,11 +6,11 @@ sudo /sbin/rcSuSEfirewall2 restart
 
 echo "ban tcp input from 172.16.1.0/24"
 
-sudo /usr/sbin/iptables -I INPUT -p TCP -s 172.16.1.0/24 -j DROP
+sudo /usr/sbin/iptables -I INPUT -p TCP -d 172.16.1.0/24 -j DROP
 
 echo "ban tcp output for 172.16.1.0/24"
 
-sudo /usr/sbin/iptables -I OUTPUT -p TCP -d 172.16.1.0/24 -j DROP
+sudo /usr/sbin/iptables -I OUTPUT -p TCP -s 172.16.1.0/24 -j DROP
 
 echo "finished banning tcp connections within 172.16.1.0/24"
 
